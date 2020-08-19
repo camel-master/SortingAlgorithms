bd7e10c443dd3a2ac9a78a0602840bc69c4302f1
MacBook2011 <rlaalfks@gmail.com>
2020-08-20 08:12:52 +0900

    heap algorithm checking completed

--- src/me/dave/HeapISorter.java
+++ src/me/dave/HeapISorter.java
@@ -6,14 +6,14 @@ public class HeapISorter extends Sorter {
 
     //private PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
     private Heap minHeap = new Heap();
-    //private Heap2 minHeap = new Heap2(10000002);
+    //private Heap2 minHeap = new Heap2();
     @Override
     public void sort(int[] arr) {
-        for(int i=0; i < arr.length-1; i++) {
+        for(int i=0; i < arr.length; i++) {
             //this.minHeap.add(arr[i]);
             this.minHeap.insert(arr[i]);
         }
-        for(int i=0; i < arr.length-1; i++) {
+        for(int i=0; i < arr.length; i++) {
             arr[i] = this.minHeap.remove();
         }
     }
