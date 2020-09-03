6d37bf5bba1753952599969781daa62d6b3414f0
MacBook2011 <rlaalfks@gmail.com>
2020-09-04 08:56:17 +0900

    Application.java is changed

--- src/me/dave/Application.java
+++ src/me/dave/Application.java
@@ -8,17 +8,17 @@ public class Application {
          *  n^2 algorithms : 100000
          *  n Log n algorithms : 10000000;
          */
-        SortingTester sTest = new SortingTester(5);
+        SortingTester sTest = new SortingTester(100000);
 
         //nlogn algorithms
-        sTest.sortingTest(SorterType.Heap);
-        sTest.sortingTest(SorterType.Quick);
+//        sTest.sortingTest(SorterType.Heap);
+//        sTest.sortingTest(SorterType.Quick);
 //        sTest.sortingTest(SorterType.Merge);
 
         //n^2 algorithms
-//        sTest.sortingTest(SorterType.Selection);
-//        sTest.sortingTest(SorterType.Insert);
-//        sTest.sortingTest(SorterType.Bubble);
+        sTest.sortingTest(SorterType.Selection);
+        sTest.sortingTest(SorterType.Insert);
+        sTest.sortingTest(SorterType.Bubble);
 
     }
 }
