9311bd4998a5370c0bb9b11f9343cee61d84bd4e
MacBook2011 <rlaalfks@gmail.com>
2020-11-14 09:04:48 +0900

    add commet

--- src/me/dave/InsertISorter.java
+++ src/me/dave/InsertISorter.java
@@ -6,7 +6,7 @@ public class InsertISorter extends Sorter {
     public void sort(int[] arr) {
         for(int i=1; i<arr.length; i++) {
             for(int j=i; j>0; j--) {
-                if(arr[j-1]>arr[j]) {
+                if(arr[j-1]>arr[j]) {   //arr[j-1]<arr[j] 로 DESC
                     swap(arr, j-1, j);
                 }
             }
